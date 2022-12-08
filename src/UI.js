3fcb782f3ae6a5c47636c5d06255bf2bc936c5d5
alex <alexthetaffer@gmail.com>
2022-12-08 11:35:19 +0300

    Realisation

--- src/UI.js
+++ src/UI.js
@@ -1,12 +1,12 @@
-export function renderInbox() {
-    const header = document.createElement('h1');
-    header.textContent = "Inbox";
+import { getInboxProject, getProjects } from "./app.js";
+import { format } from "date-fns";
 
-    const content = document.getElementById("content");
-    content.appendChild(header);
-    for (task in currentProject.tasks) {
-        content.appendChild(createTask(task));
-    }
+const header = document.getElementById('main-header');
+const content = document.getElementById("content");
+
+export function renderInbox() {
+    header.textContent = "Inbox";
+    renderTasks(getInboxProject());
     content.appendChild(createAddTaskBtn());
 }
 
@@ -26,17 +26,66 @@ function createAddTaskBtn() {
     return btn;
 }
 
+function renderNavProjects() {
+    const projects = getProjects();
+    const projectsDiv = document.querySelector('.custom-projects');
+    for (let i = 3; i < projects.length; i++) {
+        const btn = document.createElement('button');
+        btn.classList.add('nav-button');
+        btn.textContent = projects[i].name;
+        projectsDiv.appendChild(btn);
+    }
+}
+
+function renderTasks(project) {
+    const tasks = project.tasks;
+    for (let i = 0; i < tasks.length; i++) {
+        const task = createTask(tasks[i]);
+        task.setAttribute("data", i);
+        if (tasks[i].isOverdue()) {
+            task.classList.add('overdue');
+        }
+        content.appendChild(task);
+    }
+}
+
+function formatDate(dateString) {
+    const date = new Date(dateString);
+
+    return format(date, "MMM do");
+}
+
 function createTask(task) {
     const newTask = document.createElement('div');
     newTask.classList.add('task');
-    const markup = `
-    <h1>Hello there!</h1>
-    `
-    newTask.innerHTML = markup;
+    const date = (task.dueDate === undefined) ? "" : formatDate(task.dueDate);
     
+    newTask.innerHTML = `
+    <div class="left">
+            <button><img src="images/unchecked${getPriorityColor()}.svg" alt=""></button>
+            <span class="title">${task.title}</span>
+        </div>
+        <div class="right">
+            <span class="date">${date}</span>
+            <button><img src="images/edit.svg" alt=""></button>
+            <button><img src="images/delete.svg" alt=""></button>
+        </div>
+        <div class="description">
+            ${task.description}
+        </div>
+    `
+
+    function getPriorityColor() {
+        if (task.priority === undefined) return "";
+        if (task.priority === "low") return "-blue";
+        if (task.priority === "medium") return "-orange";
+        if (task.priority === "high") return "-red";
+    }
     return newTask;
 }
 
 export default function renderPage() {
     renderInbox();
+    renderNavProjects();
 }
+
---
+++ src/app.js
@@ -0,0 +1,70 @@
+import { compareAsc } from "date-fns";
+
+
+
+class Task {
+    constructor(title, description = "", priority, dueDate) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.dueDate = dueDate;
+        this.completed = false;
+    }
+
+    isOverdue() {
+        const currentDate = new Date().toISOString().slice(0, 10);
+        if (compareAsc(new Date(this.dueDate), new Date(currentDate)) == -1) {
+            return true;
+        } else return false;
+    }
+}
+
+
+class Project {
+    constructor(name, color = "#ffffff") {
+        this.name = name;
+        this.color = color;
+        this.tasks = [];
+    }
+
+    addTask(title, description, priority, dueDate) {
+        this.tasks.push(new Task(title, description, priority, dueDate));
+    }
+
+    getTasks() {
+        return this.tasks;
+    }
+}
+
+export function getProjects() {
+    return myProjects;
+}
+
+export function getProjectByName(name) {
+    myProjects.forEach(project => {
+        if (project.name === name) {
+            return project;
+        }
+    })
+}
+
+export function getInboxProject() {
+    return myProjects[0];
+}
+
+export function setProject(name, color) {
+    myProjects.push(new Project(name, color));
+}
+
+const myProjects = [];
+myProjects.push(new Project("inbox"));
+myProjects.push(new Project("today"));
+myProjects.push(new Project("Upcoming"));
+
+// Test projects
+myProjects.push(new Project("Project 1"));
+myProjects.push(new Project("Project 2"));
+
+// Test tasks
+myProjects[0].addTask("Do stuff", "Grind hard on this one.", "high", "2022-12-02");
+myProjects[0].addTask("Do other stuff", "Just takie it easy", "low", "2023-01-01");
